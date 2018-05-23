554567f8962601f58c30f881a401f478ccacf148
unknown <uptimevic@gmail.com>
2018-05-23 15:22:08 -0400

    d

--- exMeta/index.js
+++ exMeta/index.js
@@ -1,7 +1,10 @@
+#!/usr/bin/env node
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Base_1 = require("nbake/lib/Base");
 const ABase_1 = require("meta-admin/lib/ABase");
+class Example extends ABase_1.Srv {
+}
 const os = require('os');
 const fs = require('fs');
 const download = require('image-downloader');
